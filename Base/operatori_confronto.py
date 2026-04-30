cb96bb8243f6a627abe548b9b52a914f12496d97
Salisou <salisoumoussa1@gmail.com>
2026-04-30 14:43:13 +0200

    Update Gli Opeatori di confronto

--- Base/operatori_confronto.py
+++ Base/operatori_confronto.py
@@ -1,4 +1,21 @@
 
-# Gli Opeatori di confronto (Vero/falso)
-print(5 > 3) # Vero
-print(5 < 3) # Falso
+# Gli Opeatori di confronto (==, !=, <, >, <=, >=) (Vero/falso)
+
+
+print("Benvebuto nel programma\n")
+print("===================================================\n")
+print("Inserisci 2 numeri\n")
+
+num1 = int(input("Inserisci il primo numero:  👉 ")) 
+num2 = int(input("Inserisci il secondo numero: 👉 ")) 
+
+
+# Gli Opeatori di confronto (==, !=, <, >, <=, >=) (Vero/falso)
+
+print(num1 > num2)  # Vero
+print(num1 < num2)  # Falso
+print(num1 != num2) # Vero
+print(num1 == num2)
+print(num1 <= num2)
+print(num1 >= num2)
+
---
+++ Base/prova.py
@@ -0,0 +1,1 @@
+# Ecco il link del repository https://github.com/salisou/Corso_Python_edizione2.git
