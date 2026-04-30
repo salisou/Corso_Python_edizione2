84d084e54dd049fd1ecb38af1165f4060ab9d9da
Salisou <salisoumoussa1@gmail.com>
2026-04-30 18:09:51 +0200

    add esercizi

--- Base/esercizi.py
+++ Base/esercizi.py
@@ -14,3 +14,33 @@ print('tipo del nome =>', type(nome))
 print('tipo dell\'età => ', eta)
 print(type(altezza))
 print(type(studente))
+
+
+# Esercizio 1: Pari/ Dispari con (if else)
+# 1 Chiedere all'utente di inserire un numer
+# 2 Fare una verifica se il numero inserito è pari o dispari
+"""
+Numero = int(input('Inserisci un numero: '))
+
+if Numero %2 == 0:
+    print("Il numero inserito è pari")
+else:
+    print("Il numero inserito è dispari")
+"""
+    
+""" 
+    Esercizio 2: Verificare se l'utente è maggiorenne o minorenne
+"""
+
+"""
+eta = int(input("Quanti anni hai? "))
+
+if eta > 18:
+    print("Sei maggiorenne")
+elif eta == 18:
+    print("Sei maggiorenne")
+else:
+    print("Sei minorenne")
+"""    
+
+
