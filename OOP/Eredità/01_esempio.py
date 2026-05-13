89268cbd965f2c2e4140b23c65d1672f335ed01f
Salisou <salisoumoussa1@gmail.com>
2026-05-13 14:16:38 +0200

    Update Project OOP/Tkinetr

---
+++ OOP/Eredità/01_esempio.py
@@ -0,0 +1,8 @@
+"""
+    L’ereditarietà permette di:
+        - riutilizzare codice
+        - specializzare una classe base
+        - creare gerarchie logiche
+        - ridurre duplicazioni
+        - creare componenti riutilizzabili (es. widget Tkinter personalizzati)
+"""
